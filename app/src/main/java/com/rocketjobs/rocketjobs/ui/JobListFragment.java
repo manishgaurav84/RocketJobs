8cc5e99d275ac375700c6ad8f415fd85ca6ec286
manishgaurav84 <manishgaurav84@gmail.com>
2018-01-28 06:59:19 +0530

    init version

---
+++ app/src/main/java/com/rocketjobs/rocketjobs/ui/JobListFragment.java
@@ -0,0 +1,11 @@
+package com.rocketjobs.rocketjobs.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ManishG on 27-01-2018.
+ */
+
+public class JobListFragment extends Fragment {
+
+}
